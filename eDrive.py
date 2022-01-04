da8f8937ec0cd4d9e479589ea24534877999abed
jakvok <jakvok@gmail.com>
2022-01-04 17:08:41 +0100

    v3 4.1.2022
    comments

--- eDrive.py
+++ eDrive.py
@@ -9,12 +9,12 @@ class Config:
     and methods for communication.
     '''
     def __init__(self, cfile='./config.xml') -> None:
-        xml = et.parse(cfile)   # read configuration from config xml file
+        xml = et.parse(cfile)   # parse configuration from config xml file
         for node in xml.iter():
             if node.tag == 'com_port':
                 self.port = node.text   # comport for serial communication
             if node.tag == 'master_mask':
-                self.master_mask = node.text
+                self.master_mask = node.text # master's bus identification mask
             if node.tag == 'testing':   # testing mode yes/no
                 if node.text == 'True':
                     self.test = True
@@ -22,7 +22,7 @@ class Config:
                     self.test = False
             if node.tag == 'test_port': # source input file for testing
                 self.test_port = node.text
-            if node.tag == 'error_log':
+            if node.tag == 'error_log': # file to log errors
                 self.errlog = node.text
 
     def exchangeData(self, device, command, nr_bytes):
@@ -33,25 +33,25 @@ class Config:
         Output: list() of single bytes() 
         '''
         result = []
-        while not result:
+        while not result: # make communication attempts until response appears
             try:
-                if not self.test:
-                    with serial.Serial(self.port, 2400, timeout = 2) as ser:
+                if not self.test: # when not test mode, open serial connection
+                    with serial.Serial(self.port, 2400, timeout = 0) as ser:
                         ser.flushInput() #clean serial input
                         # string of device address+command retyped to bytes string and sent to serial bus
-                        ser.write(bytes(device + chr(command), 'utf-8'))
+                        ser.write(bytes(device + chr(command), 'utf-8')) # write to serial port
                         print('To device {0} was sent command {1}.'.format(device, command))
-                        time.sleep(2)
+                        time.sleep(2) # time pause until measurements done
                         picaxe = ser.readline() # read line of bytes from bus
-                else:
+                else: # when test mode is running, read bytes from file, not from serial bus
                     with open(self.test_port, 'r', encoding='utf-8') as ser:
                         time.sleep(1)
                         picaxe = bytes(ser.readline(), 'utf-8') # offline; read line from the test file and retype to bytes sequence
-                for n in range(len(picaxe)): #iterate thru given sequence of bytes and searching bytes come after 'mask'
+                for n in range(len(picaxe)): #iterate thru given sequence of bytes and searching bytes come after 'master mask'
                     x = chr(picaxe[n]) + chr(picaxe[n+1]) + chr(picaxe[n+2]) + chr(picaxe[n+3])
-                    if x == self.master_mask: # if 'mask' found, append next bytes to return list
+                    if x == self.master_mask: # if 'master mask' found, append next bytes to return list
                         try:
-                            for a in range(nr_bytes):
+                            for a in range(nr_bytes): # make list of bytes after 'mask'
                                 result.append(picaxe[n+4+a])
                         except IndexError:
                             self.__log('exchangeData(): Expected count of bytes not found.')
@@ -65,12 +65,12 @@ class Config:
         print('From device {0} come {1} bytes: {2}'.format(device, len(result), result))
         return result
 
-    def sendData(self, device, command):
+    def sendData(self, device, command): # to be completed
         pass
 
     def __log(self, text):
         '''
-        Writes text to error log
+        Writes datum&text to error log
         '''
         try:
             with open(self.errlog, 'a', encoding='utf-8') as f:
@@ -94,32 +94,35 @@ class Temperature(Config):
         self.command = command
         xml = et.parse(cfile)   # read configuration from config xml file
         for node in xml.iter():
-            if node.tag == 'temperature':
+            if node.tag == 'temperature': # settings for obj Temperature are in element 'temperature'
                 self.output = node[0].text   # output file
                 self.test_output = node[1].text # test file output
+        if self.test: # when test mode on, set output to test output file
+            self.output = self.test_output
 
     def measure(self, repeat=5):
         '''
         Method returns temperature value from sensor.
-        Input: int() number of measure repeat
+        Input: int() number of measure repeats
         Output: +-float() temperature averange value from <repeat-2> count 
         '''
-        if repeat < 3:
+        if repeat < 3: # not possible set less repeats than 3; min/max values are cut off
             self.__log('measure(): Min. <repeat> value=3. Set to 3.')
             repeat = 3
         temperatures = []
-        for _ in range(repeat):
-            datas = self.exchangeData(self.device, self.command, 2)
+        for _ in range(repeat): # repeat measurements
+            datas = self.exchangeData(self.device, self.command, 2) # send command and get 2 bytes according to DS18B20 sensor 12bit protocol
             x = self.__alignByte(datas[0]) + self.__alignByte(datas[1]) #make one str() which represents sequence of bits from input values
             temperature = 0
+            # get temp values acc to DS18B20 sensor 12bit protocol
             p = 6
-            for n in range(5,16): # sum of each bit powers according by its position 
+            for n in range(5,16): # sum of each bit powers according by it's position 
                 temperature += int(x[n])*2**p
                 p = p - 1
             if x[4] == '1': # if the fifth bit is 1, temperature is negative
                 temperature -= 128
             temperatures.append(temperature)
-        temperatures = sorted(temperatures)
+        temperatures = sorted(temperatures) # sort values in min-max order
         # cut min/max values, make averange and round
         return round((sum(temperatures[1:(repeat-1)]))/(repeat-2),1) 
 
@@ -130,12 +133,3 @@ class Temperature(Config):
                 alignByte(36) -> '00100100'
         '''
         return '0' * (8-len(str(bin(intbyte))[2:])) + str(bin(intbyte))[2:]
-
-
-
-
-
-'''
-temp = Temperature('p001', 81)
-print(temp.errlog)
-'''
--- main.py
+++ main.py
@@ -3,21 +3,18 @@
 import eDrive
 import time
 
+# create 4 objects of temperature
 outside1 = eDrive.Temperature('p001', 81)
 outside2 = eDrive.Temperature('p001', 82)
 inside = eDrive.Temperature('p002', 81)
 heat_water = eDrive.Temperature('p002', 82)
 
+# make string of actuall time and temperature values
 data = '{};{};{};{};{}\n'.format(time.strftime('%d.%m.%Y %H:%M'),outside1.measure(),outside2.measure(),inside.measure(),heat_water.measure())
 print(data)
 
-if outside1.test:
-    file = outside1.test_output
-else:
-    file = outside1.output
-
-try:
-    with open(file, 'a', encoding='utf-8') as f:
+try: # writing data to output file
+    with open(outside1.output, 'a', encoding='utf-8') as f:
         f.write(data)
 except:
-	print('Writing to file {} failed.'.format(file))
+	print('Writing to file {} failed.'.format(outside1.output))
